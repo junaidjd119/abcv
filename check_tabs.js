6b692eb8514c4b965456b6581b09221a170493db
Admin <admin@example.com>
2026-06-29 14:57:07 +0500

    Remove dev/debug/temp files - clean up for production

--- check_tabs.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('E:/gym/abcv-main/abcv-main/gym-empire-arcade.html', 'utf8');
-const tabs = ['tab-runner','tab-slam','tab-truck','tab-boxing','tab-catcher','tab-locker'];
-tabs.forEach(t => {
-    let count = (html.match(new RegExp('id="' + t + '"', 'g')) || []).length;
-    console.log(t, '- found', count, 'time(s)');
-});
